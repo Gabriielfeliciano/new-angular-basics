8f34a6861c392ae8bc8cff4fbea86513b5aede9c
Gabriielfeliciano <gabrielfeliciano@gmail.com>
2026-04-14 18:48:53 +0100

    Computed Signal

--- src/app/hello/hello.ts
+++ src/app/hello/hello.ts
@@ -1,4 +1,4 @@
-import { Component, signal } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 
 @Component({
   selector: 'app-hello',
@@ -18,6 +18,7 @@ export class Hello {
   }
 
   protected count = signal(0);
+  protected doubleCount = computed(() => this.count() * 2);
 
   protected more(){
     this.count.update(value => value + 1)
